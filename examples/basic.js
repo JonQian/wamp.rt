331a743c71e167cf53e99590bb787957112100d3
JonQian <hwq3424@qq.com>
2018-11-01 15:58:14 +0800

    格式化为tab缩进

--- examples/basic.js
+++ examples/basic.js
@@ -15,42 +15,46 @@ var program = require('commander');
 
 
 program
-    .option('-p, --port <port>', 'Server IP port', parseInt,9000);
+	.option('-p, --port <port>', 'Server IP port', parseInt, 9000);
 
 
 function onRPCRegistered(uri) {
-    console.log('onRPCRegistered RPC registered', uri);
+	console.log('onRPCRegistered RPC registered', uri);
 }
 
 function onRPCUnregistered(uri) {
-    console.log('onRPCUnregistered RPC unregistered', uri);
+	console.log('onRPCUnregistered RPC unregistered', uri);
 }
 
 function onPublish(topicUri, args) {
-    console.log('onPublish Publish', topicUri, args);
+	console.log('onPublish Publish', topicUri, args);
 }
 
 //
 // WebSocket server
 //
-var app = new Router(
-    { port: program.port,
-      // The router will select the appropriate protocol,
-      // but we can still deny the connection
-      // TODO: this should be the other way round, really ...
-      handleProtocols: function(protocols,cb) {
-          console.log(protocols);
-          cb(true,protocols[0]);
-          //cb(false);
-      }
-    }
-);
+var app = new Router({
+	port: program.port,
+	// The router will select the appropriate protocol,
+	// but we can still deny the connection
+	// TODO: this should be the other way round, really ...
+	handleProtocols: function(protocols, cb) {
+		console.log(protocols);
+		cb(true, protocols[0]);
+		//cb(false);
+	}
+});
 
 app.on('RPCRegistered', onRPCRegistered);
 app.on('RPCUnregistered', onRPCUnregistered);
 app.on('Publish', onPublish);
 
-app.regrpc('wamp.rt.foo', function(id,args) {
-    console.log('called with ' + args);
-    app.resrpc(id,null,[["bar", "bar2"], {"key1": "bar1", "key2": "bar2"}]);
+app.regrpc('wamp.rt.foo', function(id, args) {
+	console.log('called with ' + args);
+	app.resrpc(id, null, [
+		["bar", "bar2"], {
+			"key1": "bar1",
+			"key2": "bar2"
+		}
+	]);
 });
